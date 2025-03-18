a020c4c190bb21668e504421406e39f7ca343c8b
CounterDag <152663355+CounterDag@users.noreply.github.com>
2025-03-19 10:17:32 +1100

    Delete Scripts directory

--- Scripts/Main.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-#selection code
-#when left clicked, move piecemarker from piecepositionplaced to piecepositionselected
-#Then, highlight all legal moves.
-#if canceled (right click? Cancel button?), move from piece positionselected to piecepositionplaced
-#if a legal move is selected, do move
-#otherwise, do nothing
-#Main needs to know what legal moves are. That should be passed from the pieces themselves.
